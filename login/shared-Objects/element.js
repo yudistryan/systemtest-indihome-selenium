0d078be6e1f1e26539114983077c0ddc89d66622
yudistryan <yudistryan@users.noreply.github.com>
2020-12-22 20:47:55 +0700

    Penambahan test report

---
+++ login/shared-Objects/element.js
@@ -0,0 +1,6 @@
+module.exports = {
+    loading: "//h3[normalize-space()='Sedang Diproses']",
+    verifying: "//h1[normalize-space()='Verifikasi Akun'] ']",
+    unmatch: "//small[normalize-space()='Maaf konfirmasi password tidak sama.']",
+    failed: "//h3[normalize-space()='Gagal']"
+}
